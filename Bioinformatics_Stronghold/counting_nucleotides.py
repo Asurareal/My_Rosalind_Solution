22f1edb5b6e259ab9b89fefda2e09f4dbfa03c25
Eklavya Upadhyay <aviarycrow@gmail.com>
2026-06-04 02:11:08 +0530

    Rename parameter from filename to dna in function

--- Bioinformatics_Stronghold/counting_nucleotides.py
+++ Bioinformatics_Stronghold/counting_nucleotides.py
@@ -1,7 +1,7 @@
-def counting_nucleotides(filename):
+def counting_nucleotides(dna):
   nucleotides = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
   for nucleotide, value in nucleotides.items():
-      nucleotides[nucleotide] = filename.count(nucleotide)
+      nucleotides[nucleotide] = filename.count(dna)
   return nucleotides
 if __name__ == '__main__':
     print(counting_nucleotides('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'))
