b632c511a431987b0117a790c090949865b15b56
Eklavya Upadhyay <aviarycrow@gmail.com>
2026-06-04 18:17:28 +0530

    Implement FASTA file parsing function
    Added a function to parse FASTA files and extract sequences.

--- Bioinformatics_Stronghold/utility.py
+++ Bioinformatics_Stronghold/utility.py
@@ -1,6 +1,18 @@
 def file_txt(path):
   with open(path, 'r') as f:
     return f.read().strip()
-    
+
+def parse_fasta(path):
+    with open(path, 'r') as f:
+        raw_file = f.read().strip()
+    sequences = {}
+    chunks = raw_file.split(">")[1:]
+    for chunk in chunks:
+        lines = chunk.splitlines()       
+        header = lines[0]                
+        dna_sequence = "".join(lines[1:]) 
+        sequences[header] = dna_sequence 
+    return sequences
 if __name__ == "__main__":
+    print(parse_fasta("/content/rosalind_gc.txt"))
     print(file_txt("/content/rosalind_rna.txt"))
